8bae031bff21127f316082099d5768b999c8df35
Quang Truong <truongsyquang1202@gmail.com>
2024-01-30 01:50:59 +0700

    edit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,12 @@
 // header.jsx
 import React from "react";
 import "./Header.css";
-import FPTlogo from "./img/FPT_logo.png";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="content">
         <div className="navbar">
-          <img src={FPTlogo} alt="FPT Software" />
           <div className="actions">
             <a href="#!" className="btn action-btn">
               Sign In
